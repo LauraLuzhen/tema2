55f62d17885c9fe24df03d3e45be3d1549466513
laura <laura@192.168.1.132>
2024-10-13 23:31:35 +0200

    Tema 2 Actividades switch Ejercicios 1-5

--- Tema2/src/actswitch/Ejercicio4.java
+++ Tema2/src/actswitch/Ejercicio4.java
@@ -6,24 +6,84 @@ import java.util.Scanner;
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		
-		//Creamos el Scanner
-		Scanner reader = new Scanner (System.in);
-		
-		//Declaración de variables
-		//Variable que guarda la primera tirada
+
+		// Creamos el Scanner
+		Scanner reader = new Scanner(System.in);
+
+		// Declaración de variables
+		// Variable que guarda la primera tirada
 		int tirada1;
-		//Variable que guarda la segunda tirada
+		// Variable que guarda la segunda tirada
 		int tirada2;
-		//Variable que guarda la respuesta del usuario
+		// Variable que guarda la respuesta del usuario
 		String respuesta1;
-		//Variable que guarda la respuesta del usuario
+		// Variable que guarda la respuesta del usuario
 		String respuesta2;
-		
-		//Le preguntamos al uruaio cuánto ha sacado en la primera tirada
-		System.out.println("¿Cuánto has sacado en la primera tirada? (Indicar el número ");
+		// Variable que guarda la suma de ambas tiradas
+		int suma;
+
+		// Le preguntamos al uruaio cuánto ha sacado en la primera tirada
+		System.out
+				.println("¿Cuánto has sacado en la primera tirada? (Indicar el número con letras y en mayúsucalas): ");
 		respuesta1 = reader.next();
-		
-		//Creamos un switch para guardar el valor del dado en la primera tirada
+
+		// Le preguntamos al uruaio cuánto ha sacado en la segunda tirada
+		System.out.println("¿Cuánto has sacado en la segunda tirada? (Indicar el número con letras y en mayúsculas): ");
+		respuesta2 = reader.next();
+
+		// Creamos un switch para guardar el valor del dado en la primera tirada
+		tirada1 = switch (respuesta1) {
+		case "UNO" -> {
+			yield 1;
+		}
+		case "DOS" -> {
+			yield 2;
+		}
+		case "TRES" -> {
+			yield 3;
+		}
+		case "CUATRO" -> {
+			yield 4;
+		}
+		case "CINCO" -> {
+			yield 5;
+		}
+		case "SEIS" -> {
+			yield 6;
+		}
+		default -> throw new IllegalArgumentException("Unexpected value: " + respuesta1);
+		};
+
+		// Creamos un switch para guardar el valor del dado en la primera tirada
+		tirada2 = switch (respuesta2) {
+		case "UNO" -> {
+			yield 1;
+		}
+		case "DOS" -> {
+			yield 2;
+		}
+		case "TRES" -> {
+			yield 3;
+		}
+		case "CUATRO" -> {
+			yield 4;
+		}
+		case "CINCO" -> {
+			yield 5;
+		}
+		case "SEIS" -> {
+			yield 6;
+		}
+		default -> throw new IllegalArgumentException("Unexpected value: " + respuesta2);
+		};
+
+		// Sumamos el resultado de ambas tiradas
+		suma = tirada1 + tirada2;
+
+		// Imprimimos la suma de ambas tiradas por pantalla
+		System.out.println("La suma entre las dos tiradas es: " + suma);
+
+		// Cerramos el Scanner
+		reader.close();
 	}
 }
--- Tema2/src/actswitch/Ejericico5.java
+++ Tema2/src/actswitch/Ejericico5.java
@@ -1,5 +1,35 @@
 package actswitch;
 
+//Importamos el Scanner
+import java.util.Scanner;
+
 public class Ejericico5 {
 
+	public static void main(String[] args) {
+
+		// Creamos el Scanner
+		Scanner reader = new Scanner(System.in);
+
+		// Declaración de variables
+		// Variable que guarda la letra del tipo de carnet
+		String tipoCarnet;
+
+		// Le preguntamos al usuario qué tipo de carnet tiene
+		System.out.println("¿Qué tipo de carnet tiene? (E, D, C1-C5, A, B1-B2): ");
+		tipoCarnet = reader.next();
+
+		// Creamos un switch que imprima por pantalla el tipo de vehículo que puede
+		// conducir según la letra del carnet que tenga el usuario
+		switch (tipoCarnet) {
+		case "E" -> System.out.println("Remolques");
+		case "D" -> System.out.println("Autobuses");
+		case "C1", "C2", "C3", "C4", "C5" -> System.out.println("Camiones");
+		case "A" -> System.out.println("Motocicletas");
+		case "B1", "B2" -> System.out.println("Automóviles");
+		default -> System.out.println("Categoría no contemplada");
+		}
+
+		// Cerramos la consola
+		reader.close();
+	}
 }
