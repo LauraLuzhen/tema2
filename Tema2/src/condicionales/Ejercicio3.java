abee449e1ce12f6ef57b853266a534e7a43f67bd
ll.rodriguez <ll.rodriguez@106-26.info2>
2024-10-14 12:39:19 +0200

    Tema 2 Actividades condicionales Ejercicios 1-8

--- Tema2/src/condicionales/Ejercicio3.java
+++ Tema2/src/condicionales/Ejercicio3.java
@@ -6,6 +6,18 @@ public class Ejercicio3 {
 
 	public static void main(String[] args) {
 
+		/*
+		 * PRUEBAS MÍNIMAS
+		 * 
+		 * Prueba 1: introducimos un nº mayor a 8 cifras o menor a 8 cifras, esperamos
+		 * que imprima el mensaje de error. Por ejemplo, 12 debe imprimir el mensaje
+		 * "El número introducido no corresponde a los de un DNI.".
+		 * 
+		 * Prueba 2: introducimos un número válido para un DNI (8 cifras), debe calcular
+		 * la letra correspondiente e imprimirla por pantalla. Por ejemplo, introducimos
+		 * 11111111 le corresponde la letra H.
+		 */
+
 		// Creamos el Scanner
 		Scanner reader = new Scanner(System.in);
 
@@ -33,11 +45,82 @@ public class Ejercicio3 {
 				yield "T";
 			}
 			case 1 -> {
-
+				yield "R";
+			}
+			case 2 -> {
+				yield "W";
+			}
+			case 3 -> {
+				yield "A";
+			}
+			case 4 -> {
+				yield "G";
+			}
+			case 5 -> {
+				yield "M";
+			}
+			case 6 -> {
+				yield "Y";
+			}
+			case 7 -> {
+				yield "F";
+			}
+			case 8 -> {
+				yield "P";
+			}
+			case 9 -> {
+				yield "D";
+			}
+			case 10 -> {
+				yield "X";
+			}
+			case 11 -> {
+				yield "B";
+			}
+			case 12 -> {
+				yield "N";
+			}
+			case 13 -> {
+				yield "J";
+			}
+			case 14 -> {
+				yield "Z";
+			}
+			case 15 -> {
+				yield "S";
+			}
+			case 16 -> {
+				yield "Q";
+			}
+			case 17 -> {
+				yield "V";
+			}
+			case 18 -> {
+				yield "H";
+			}
+			case 19 -> {
+				yield "L";
+			}
+			case 20 -> {
+				yield "C";
+			}
+			case 21 -> {
+				yield "K";
+			}
+			case 22 -> {
+				yield "E";
+			}
+			default -> {
+				yield "El número introducido no corresponde a los de un DNI";
 			}
 			};
+			// Imprimimos la letra que le pertenece al DNI
+			System.out.println("El DNI " + dni + " corresponde con la letra " + letra);
 		} else {
 			System.out.println("El número introducido no corresponde a los de un DNI.");
 		}
+
+		// Cerramos el Scanner
+		reader.close();
 	}
 }
--- Tema2/src/condicionales/Ejercicio4.java
+++ Tema2/src/condicionales/Ejercicio4.java
@@ -1,5 +1,158 @@
 package condicionales;
 
+//Importamos el Scanner
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
+	public static void main(String[] args) {
+
+		// Creamos el Scanner
+		Scanner reader = new Scanner(System.in);
+
+		// Declaración de variables
+		// Variable que guarda el número introducido por el usuario
+		int num;
+		// Variable que calcule las unidades
+		int unidades;
+		// Variable que calcule las decenas
+		int decenas;
+		// Variable que guarda el valor de la unidad
+		String valorUnidades;
+		// Variable que guarda el valor de la decena
+		String valorDecenas;
+
+		// Le pedimos un número al usuario
+		System.out.println("Introduce un número entero entre el 1-99: ");
+		num = reader.nextInt();
+
+		// Creamos un if-else para comprobar que le número introducido está dentro del
+		// rango establecido
+		if (num <= 99 && num >= 1) {
+			// Creamos un switch que según el valor lo imprima con letras por pantalla
+			switch (num) {
+			case 1 -> System.out.println("uno");
+			case 2 -> System.out.println("dos");
+			case 3 -> System.out.println("tres");
+			case 4 -> System.out.println("cuatro");
+			case 5 -> System.out.println("cinco");
+			case 6 -> System.out.println("seis");
+			case 7 -> System.out.println("siete");
+			case 8 -> System.out.println("ocho");
+			case 9 -> System.out.println("nueve");
+			case 10 -> System.out.println("diez");
+			case 11 -> System.out.println("once");
+			case 12 -> System.out.println("doce");
+			case 13 -> System.out.println("trece");
+			case 14 -> System.out.println("catorce");
+			case 15 -> System.out.println("quince");
+			default -> {
+				decenas = num / 10;
+				unidades = num % 10;
+				valorDecenas = switch (decenas) {
+				case 1 -> {
+					yield "dieci";
+				}
+				case 2 -> {
+					if (unidades == 0) {
+						yield "veinte";
+					} else {
+						yield "veinti";
+					}
+				}
+				case 3 -> {
+					if (unidades == 0) {
+						yield "treinta";
+					} else {
+						yield "treinta y ";
+					}
+				}
+				case 4 -> {
+					if (unidades == 0) {
+						yield "cuarenta";
+					} else {
+						yield "cuarenta y ";
+					}
+				}
+				case 5 -> {
+					if (unidades == 0) {
+						yield "cincuenta";
+					} else {
+						yield "cincuenta y ";
+					}
+				}
+				case 6 -> {
+					if (unidades == 0) {
+						yield "sesenta";
+					} else {
+						yield "sesenta y ";
+					}
+				}
+				case 7 -> {
+					if (unidades == 0) {
+						yield "setenta";
+					} else {
+						yield "setenta y ";
+					}
+				}
+				case 8 -> {
+					if (unidades == 0) {
+						yield "ochenta";
+					} else {
+						yield "ochenta y ";
+					}
+				}
+				case 9 -> {
+					if (unidades == 0) {
+						yield "noventa";
+					} else {
+						yield "noventa y ";
+					}
+				}
+				default -> {
+					yield "El valor introducido es erróneo.";
+				}
+				};
+				valorUnidades = switch (unidades) {
+				case 1 -> {
+					yield "uno";
+				}
+				case 2 -> {
+					yield "dos";
+				}
+				case 3 -> {
+					yield "tres";
+				}
+				case 4 -> {
+					yield "cuatro";
+				}
+				case 5 -> {
+					yield "cinco";
+				}
+				case 6 -> {
+					yield "seis";
+				}
+				case 7 -> {
+					yield "siete";
+				}
+				case 8 -> {
+					yield "ocho";
+				}
+				case 9 -> {
+					yield "nueve";
+				}
+				default -> {
+					yield "El valor introducido es erróneo.";
+				}
+				};
+				System.err.println(decenas + unidades);
+			}
+			}
+		} else {
+			System.out.println("El valor introducido es erróneo.");
+		}
+
+		// Cerramos el Scanner
+		reader.close();
+	}
 }
