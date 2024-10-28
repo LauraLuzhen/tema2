6e1925801c85a7fa961db43f4bd530c46f3a1b5d
laura <laura@10.100.219.219>
2024-10-28 19:25:31 +0100

    Tema 2 Actividades for Ejercicio 8 sin repetición de for

--- Tema2/src/actfor/Ejercicio8.java
+++ Tema2/src/actfor/Ejercicio8.java
@@ -12,30 +12,34 @@ public class Ejercicio8 {
 		int numA;
 		// Variable que guarda el número B
 		int numB;
+		// Variable número más grande
+		int numMayor;
+		// Variable número más pequeño
+		int numMenor;
 
 		// Creamos el Scanner
 		Scanner reader = new Scanner(System.in);
 
-		// Le pedimos dos números enteros al usuario
-		System.out.println("Introduce el número A: ");
-		numA = reader.nextInt();
-		System.out.println("Introduce el número B: ");
-		numB = reader.nextInt();
+		// Le pedimos dos números enteros al usuario y que no sean negativos ni iguales
+		do {
+			System.out.println("Introduce el número A: ");
+			numA = reader.nextInt();
+			System.out.println("Introduce el número B: ");
+			numB = reader.nextInt();
+		} while (numA < 0 || numB < 0 || numA == numB);
 
 		// Creamos un if-else que comprube cuál es el número más grande entre A y B
 		if (numA < numB) {
-			// Si B es mayor que A imprme los números de A a B
-			for (int i = numA; i <= numB; i++) {
-				System.out.println(i);
-			}
-		} else if (numA > numB) {
-			// Si A es mayor que B imprime los números de B a A
-			for (int i = numB; i <= numA; i++) {
-				System.out.println(i);
-			}
+			numMenor = numA;
+			numMayor = numB;
 		} else {
-			// Si ambos números son iguales indicarlo
-			System.out.println("Introduce dos números diferentes");
+			numMenor = numB;
+			numMayor = numA;
+		}
+
+		// Creamos un for que imprima del número menor al mayor uno por uno
+		for (int i = numMenor; i <= numMayor; i++) {
+			System.out.println(i);
 		}
 
 		// Cerramos el Scanner
